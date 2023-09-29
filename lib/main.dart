c23b15f439fddc97afcaddbfdafbf2c3dd5450b1
Raihan Syah <raihansyah45@gmail.com>
2023-09-29 13:11:17 +0700

    push

--- lib/main.dart
+++ lib/main.dart
@@ -10,61 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const Scaffold(
-          body: Center(child: BiggerText(text: "Hello World")),
-        ));
-  }
-}
-
-class Heading extends StatelessWidget {
-  final String text;
-
-  const Heading({Key? key, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const FirstScreen(), // Panggil FirstScreen di sini
     );
   }
 }
 
-class BiggerText extends StatefulWidget {
-  final String text;
-
-  const BiggerText({Key? key, required this.text}) : super(key: key);
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: const Text("Perbesar"),
-          onPressed: () {
-            setState(() {
-              _textSize = 32.0;
-            });
-          },
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("First Screen"),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.search, color: Colors.white),
+              onPressed: () {},
+            )
+          ],
+          leading: IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )),
+      body: const Center(
+        child: Text("Hello World!"),
+      ),
+      floatingActionButton:
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
     );
   }
 }
